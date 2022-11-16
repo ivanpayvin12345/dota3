4b76d188d1690999fcade6ffcdc0c95cb579438f
ivanpayvin12345 <115572877+ivanpayvin12345@users.noreply.github.com>
2022-11-16 20:51:40 +0200

    Add files via upload

---
+++ sosi/js/popup.js
@@ -0,0 +1,55 @@
+const button1 = document.querySelector('.button1');
+const el1=document.querySelector('.btnforpopup1')
+button1.onclick = function(){
+    el1.style.cssText=`
+    --imgofher1: url(../imgforpick/power/Abaddon_icon.png);
+    --border:none;
+    `
+}
+
+const button11 = document.querySelector('.button11');
+const el11=document.querySelector('.btnforpopup1')
+button11.onclick = function(){
+    el11.style.cssText=`
+    --imgofher1: url(../imgforpick/power/Abaddon_icon.png);
+    --border:none;
+    `
+}
+
+
+const button2 = document.querySelector('.button2');
+const el2=document.querySelector('.btnforpopup2')
+button2.onclick = function(){
+    el2.style.cssText=`
+    --imgofher2: url(../imgforpick/power/Abaddon_icon.png);
+    --border:none;
+    `
+}
+
+
+const button3 = document.querySelector('.button3');
+const el3=document.querySelector('.btnforpopup3')
+button3.onclick = function(){
+    el3.style.cssText=`
+    --imgofher3: url(../imgforpick/power/Abaddon_icon.png);
+    --border:none;
+    `
+}
+
+const button4 = document.querySelector('.button4');
+const el4=document.querySelector('.btnforpopup4')
+button4.onclick = function(){
+    el4.style.cssText=`
+    --imgofher4: url(../imgforpick/power/Abaddon_icon.png);
+    --border:none;
+    `
+}
+
+const button5 = document.querySelector('.button5');
+const el5=document.querySelector('.btnforpopup5')
+button5.onclick = function(){
+    el5.style.cssText=`
+    --imgofher5: url(../imgforpick/power/Abaddon_icon.png);
+    --border:none;
+    `
+}
